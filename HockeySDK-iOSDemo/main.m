ede4dcd17fe3fe798fd34a9f8a7bdadcd4aeb506
Andreas Linde <mail@andreaslinde.de>
2012-05-01 19:04:46 +0200

    Initial Commit

---
+++ HockeySDK-iOSDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HockeySDK-iOSDemo
+//
+//  Created by Andreas Linde on 01.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CNSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([CNSAppDelegate class]));
+  }
+}
