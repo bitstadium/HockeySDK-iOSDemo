ca0a7a3de88f1e0afe668ea842ed03f79bc5ee09
Stefan Haubold <stefan.haubold@hait.de>
2014-09-24 16:08:22 +0200

    added basic today extension that triggers a crash.

---
+++ iOSDemo-Today/TodayViewController.m
@@ -0,0 +1,53 @@
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+#import "HockeySDK.h"
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@property (nonatomic, assign) BOOL didSetupHockeySDK;
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+    if (!self.didSetupHockeySDK) {
+      [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"fd51a3647d651add2171dd59d3b6e5ec"
+                                                             delegate:nil];
+      [BITHockeyManager sharedHockeyManager].authenticator.authenticationSecret = @"cdfc46d2c9e8a2f64890a6bb1337eef0";
+      [BITHockeyManager sharedHockeyManager].authenticator.identificationType = BITAuthenticatorIdentificationTypeDevice;
+      [BITHockeyManager sharedHockeyManager].authenticator.restrictApplicationUsage = NO;
+  
+      // optionally enable logging to get more information about states.
+      [BITHockeyManager sharedHockeyManager].debugLogEnabled = YES;
+      [BITHockeyManager sharedHockeyManager].crashManager.crashManagerStatus = BITCrashManagerStatusAutoSend;
+      
+  
+      [[BITHockeyManager sharedHockeyManager] startManager];
+      self.didSetupHockeySDK = YES;
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+  // Perform any setup necessary in order to update the view.
+  
+  // If an error is encountered, use NCUpdateResultFailed
+  // If there's no update required, use NCUpdateResultNoData
+  // If there's an update, use NCUpdateResultNewData
+  
+  completionHandler(NCUpdateResultNewData);
+}
+
+- (IBAction)testCrashTapped:(id)sender {
+  /* Trigger a crash */
+  NSArray *array = [NSArray array];
+  [array objectAtIndex:23];
+}
+
+@end
